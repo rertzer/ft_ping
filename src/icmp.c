83ba4158dc18a3581efaed1f2b212a77abce72c6
rertzer <rertzer@student.42.fr>
2025-12-18 09:29:35 +0100

    fix: checksum check output ok

--- src/icmp.c
+++ src/icmp.c
@@ -34,7 +34,7 @@ uint16_t check_checksum(struct icmp* icmp) {
 	uint16_t received_checksum = icmp->icmp_cksum;
 	icmp->icmp_cksum = 0;
 	uint16_t computed_checksum = get_icmp_checksum((uint16_t*)icmp);
-	return (received_checksum & computed_checksum);
+	return (received_checksum & ~computed_checksum);
 }
 icmp_info_t init_icmp_info(const char* hostname) {
 	icmp_info_t icmp_info;
